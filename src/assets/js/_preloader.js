2940f7ec38bde6f01f2b644620e1ad55f1904a3b
Etidou <45783736+Etidou@users.noreply.github.com>
2020-10-14 10:01:16 +0200

    done

--- src/assets/js/_preloader.js
+++ src/assets/js/_preloader.js
@@ -13,33 +13,44 @@ function preloaderAnimation() {
         })
         let tl = new TimelineMax();
         tl
-            .fromTo(svgright, {
-                x: '150%'
-            }, {
-                x: '0%',
-                duration: 1.5
-            })
+        .fromTo(svgright, {
+            x: '150%'
+        }, {
+            x: '0%',
+            duration: 1.5
+        })
 
-            .fromTo(svgleft, {
-                x: '-150%'
-            }, {
-                x: '0%',
-                duration: 1.5
-            }, '-=1.5')
+        .fromTo(svgleft, {
+            x: '-150%'
+        }, {
+            x: '0%',
+            duration: 1.5
+        }, '-=1.5')
 
-            .fromTo(svgx, {
-                opacity: 0
-            }, {
-                opacity: 1,
-                duration: 1.5
-            }, '-=0.75')
+        .fromTo(svgx, {
+            opacity: 0
+        }, {
+            opacity: 1,
+            duration: 1.5
+        }, '-=0.75')
 
 
         function drawTheX() {
 
+/*            new Vivus('x', {
+                file: '../../assets/svg/x.svg',
+              onReady: function (myVivus) {
+    // `el` property is the SVG element
+    myVivus.el.setAttribute('height', 'auto');
+}
+});*/
+
+
+
             new Vivus('x', {
                 start: 'autostart',
                 type: 'scenario',
+                file: '../../assets/svg/x.svg',
                 animTimingFunction: Vivus.EASE
             })
         }
@@ -47,15 +58,15 @@ function preloaderAnimation() {
         drawTheX();
 
         tl
-            .to(preloaderOverlay, 1.2, {
-                y: '0%',
-                ease: Power4.easeInOut,
-            }, '+=0.25')
+        .to(preloaderOverlay, 1.2, {
+            y: '0%',
+            ease: Power4.easeInOut,
+        }, '+=0.25')
 
-            .to(preloader, 1.5, {
-                y: '100%',
-                ease: Power4.easeInOut,
-            }, '-=1')
+        .to(preloader, 1.5, {
+            y: '100%',
+            ease: Power4.easeInOut,
+        }, '-=1')
     });
 }
 
