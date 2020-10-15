cacaefbc025d1ed3a156848e16548b9db2e2e844
Orphée Besson <orphee.besson@etudiant.univ-lr.fr>
2020-10-15 22:39:24 +0200

    optimize code

--- src/assets/js/_theme-mode.js
+++ src/assets/js/_theme-mode.js
@@ -1,20 +1,14 @@
 const switcherDark = document.querySelector('.switcher__dark');
 const switcherLight = document.querySelector('.switcher__light');
-const darklogo = document.querySelector(".darklogo");
-const lightlogo = document.querySelector(".whitelogo");
 
 function dark_mode() {
     document.body.classList.remove('body--light');
     document.body.classList.add('body--dark');
-    lightlogo.classList.remove("light-none");
-    darklogo.classList.add("dark-none");
 }
 
 function light_mode() {
     document.body.classList.remove('body--dark');
     document.body.classList.add('body--light');
-    darklogo.classList.remove("dark-none");
-    lightlogo.classList.add("light-none");
 }
 
 switcherDark.addEventListener("click", dark_mode);
