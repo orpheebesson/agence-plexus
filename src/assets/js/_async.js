ea41036581fb070b0228284e20c691904b9a5026
Etidou <45783736+Etidou@users.noreply.github.com>
2020-12-07 15:33:01 +0100

    Adress + mail + form + bones+ Async

--- src/assets/js/_async.js
+++ src/assets/js/_async.js
@@ -181,69 +181,6 @@ function membersCarousel() {
 membersCarousel();
 
 
-function bones() 
-{
-
-
-var scene, camera,renderer;
-
-
-scene = new THREE.Scene();
-scene.background = new THREE.Color(0x000000)
-
-
-camera = new THREE.PerspectiveCamera(25, window.innerWidth/window.innerHeight);
-camera.position.set(0,100,1000);
-
-renderer = new THREE.WebGLRenderer({canvas: document.querySelector(".bones canvas")});
-
-
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-controls.update();
-var abint = new THREE.AmbientLight(0xffffff,1)
-scene.add(abint)
-
-const loader = new THREE.GLTFLoader();
-
-loader.load('hum/scene.gltf', function ( gltf ) {
-
-scene.add(gltf.scene);
-
-})
-
-
-/*var objLoader = new THREE.OBJLoader();
-
-objLoader.load('full/second/test.json', function(object) {
-    scene.add(object);
-})*/
-
-
-function resizeCanvasToDisplaySize() {
-  const canvas = renderer.domElement;
-  const width = canvas.clientWidth;
-  const height = canvas.clientHeight;
-  if (canvas.width !== width ||canvas.height !== height) {
-    // you must pass false here or three.js sadly fights the browser
-    renderer.setSize(width, height, false);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-
-    // set render target sizes here
-  }
-}
-
-function animate(){
-    resizeCanvasToDisplaySize();
-    requestAnimationFrame(animate);
-    renderer.render(scene,camera);
-}
-animate()
-}
-
-bones();
-
 function delay(n) {
     n = n || 2000;
     return new Promise((done) => {
@@ -345,3 +282,67 @@ $(function () {
         }]
     });
 });
+
+
+function bones() 
+{
+
+
+var scene, camera,renderer;
+
+
+scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000)
+
+
+camera = new THREE.PerspectiveCamera(25, window.innerWidth/window.innerHeight);
+camera.position.set(0,100,1000);
+
+renderer = new THREE.WebGLRenderer({canvas: document.querySelector(".bones canvas")});
+
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+controls.update();
+var abint = new THREE.AmbientLight(0xffffff,1)
+scene.add(abint)
+
+const loader = new THREE.GLTFLoader();
+
+loader.load('assets/3DObject/scene.gltf', function ( gltf ) {
+
+scene.add(gltf.scene);
+
+})
+
+
+/*var objLoader = new THREE.OBJLoader();
+
+objLoader.load('full/second/test.json', function(object) {
+    scene.add(object);
+})*/
+
+
+function resizeCanvasToDisplaySize() {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  if (canvas.width !== width ||canvas.height !== height) {
+    // you must pass false here or three.js sadly fights the browser
+    renderer.setSize(width, height, false);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    // set render target sizes here
+  }
+}
+
+function animate(){
+    resizeCanvasToDisplaySize();
+    requestAnimationFrame(animate);
+    renderer.render(scene,camera);
+}
+animate()
+}
+
+bones();
